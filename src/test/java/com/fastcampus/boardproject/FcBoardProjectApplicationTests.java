4645b9df5faba4fc960fdbd4d2e9f8e127e481ce
Yejin Won <wonyj0228@naver.com>
2022-11-20 16:40:26 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화.
    들어간 초기 기술 : 
    
    * spring web
    * spring actuator
    * lombok
    * spring boot devtools

---
+++ src/test/java/com/fastcampus/boardproject/FcBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
