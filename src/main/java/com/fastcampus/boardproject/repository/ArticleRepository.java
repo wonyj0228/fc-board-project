093dd5d541773bbc68fe88e7427a5a38c28232e9
Yejin Won <wonyj0228@naver.com>
2022-11-28 16:42:52 +0900

    #1 - EOF 추가

--- src/main/java/com/fastcampus/boardproject/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/boardproject/repository/ArticleRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 //command + shift + t = new test
 @RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    
 }
